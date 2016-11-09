0c714d86b669342879820596264455f9c88230a4
mtmtkj <mtmt101@usingstd.com>
2016-11-09 17:38:05 +0900

    Compile js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,9 +12,3 @@ require('./bootstrap');
  * the body of the page. From here, you may begin adding components to
  * the application, or feel free to tweak this setup for your needs.
  */
-
-Vue.component('example', require('./components/Example.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
