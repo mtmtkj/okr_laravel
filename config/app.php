eb7fd3f94be80d2c5efaadb2992ce2d237dd4b46
mtmtkj <mtmt101@usingstd.com>
2016-11-09 11:24:14 +0900

    Add laravelcollective/html

--- config/app.php
+++ config/app.php
@@ -166,8 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-
-        //
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +225,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
