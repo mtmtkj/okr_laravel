4bb0987557df38424faa4eb83f47bb87c93bd9d0
hanahiroAze <hanai@hitomedia.jp>
2016-12-04 13:40:05 +0900

    delete constructor to do same in routes/web.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,16 +9,6 @@ use App\Individual;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Http\Response
