1ac6e55b3d06a4798264666c34f418295f0cf300
hanahiroAze <hanai@hitomedia.jp>
2016-12-04 15:24:11 +0900

    modify on click logo redirection / -> /home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'OKR') }}</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
@@ -36,8 +36,8 @@
               </button>
 
               <!-- Branding Image -->
-              <a class="navbar-brand" href="{{ url('/') }}">
-                {{ config('app.name', 'Laravel') }}
+              <a class="navbar-brand" href="{{ url('/home') }}">
+                {{ config('app.name', 'OKR') }}
               </a>
             </div>
 
