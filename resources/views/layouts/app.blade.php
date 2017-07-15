9d6e3b1ffb0aeb799f68c18c65ae1c740ee3ae2c
mtmtkj <mtmt101@usingstd.com>
2017-07-15 21:38:29 +0900

    Change application name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'OKR') }}</title>
+    <title>{{ config('app.name', 'Stal') }}</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
@@ -37,7 +37,7 @@
 
               <!-- Branding Image -->
               <a class="navbar-brand" href="{{ url('/home') }}">
-                {{ config('app.name', 'OKR') }}
+                {{ config('app.name', 'Stal') }}
               </a>
             </div>
 
