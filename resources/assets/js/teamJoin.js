3842ddd5109b48e38a8fd55bcc1f209dcec5307a
mtmtkj <mtmt101@usingstd.com>
2017-11-04 11:36:11 +0900

    Rewrite in ES6

--- resources/assets/js/teamJoin.js
+++ resources/assets/js/teamJoin.js
@@ -4,31 +4,31 @@ const vm = new Vue({
     name: null,
     teams: []
   },
-  created: function () {
-    const individualId = document.getElementById('individual_id').getAttribute('value');
-    this.axios.get('/api/team', {params: {individualId: individualId}})
-      .then(function (res) {
+  created () {
+    const individualId = document.getElementById("individual_id").getAttribute("value");
+    this.axios.get("/api/team", {params: {individualId: individualId}})
+      .then((res) => {
         this.teams = res.data;
-      }.bind(this))
+      })
     },
   methods: {
-    createNewTeam: function () {
-      this.axios.post('/api/team', {name: this.name})
-        .then(function (res) {
+    createNewTeam () {
+      this.axios.post("/api/team", {name: this.name})
+        .then((res) => {
           let team = res.data;
           team.joined = 0;
           this.teams.push(team);
-        }.bind(this));
+        });
     },
-    joinTheTeam: function() {
-      const teamId = document.getElementById('js-btn-join').getAttribute('data-id');
-      this.axios.post('/api/team/join', {teamId: teamId})
-        .then(function (res) {
+    joinTheTeam () {
+      const teamId = document.getElementById("js-btn-join").getAttribute("data-id");
+      this.axios.post("/api/team/join", {teamId: teamId})
+        .then(() => {
           let team = this.teams.find(function (el) {
             return el.id === parseInt(teamId);
           });
           team.joined = 1;
-        }.bind(this))
+        })
     }
   }
 });
