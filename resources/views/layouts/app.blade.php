f8cdf59e0956ceaaea035dccd9ac9e8d91ce0d46
mtmtkj <mtmt101@usingstd.com>
2017-12-03 18:45:23 +0900

    Modify styles in layouts.app for materialize

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,7 @@
     <title>{{ config('app.name', 'Stal') }}</title>
 
     <!-- Styles -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="/css/app.css" rel="stylesheet">
 
     <!-- Scripts -->
@@ -23,62 +24,45 @@
   <body>
     <div id="app">
       <header>
-        <nav class="navbar navbar-default navbar-static-top">
-          <div class="container">
-            <div class="navbar-header">
-
-              <!-- Collapsed Hamburger -->
-              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-
+        <div class="navbar-fixed">
+          <nav class="nav-wrapper">
+            <div class="container">
               <!-- Branding Image -->
-              <a class="navbar-brand" href="{{ url('/home') }}">
+              <a class="brand-logo" href="{{ url('/home') }}">
                 {{ config('app.name', 'Stal') }}
               </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-              <!-- Left Side Of Navbar -->
-              <ul class="nav navbar-nav">
-                &nbsp;
-              </ul>
 
               <!-- Right Side Of Navbar -->
-              <ul class="nav navbar-nav navbar-right">
+              <ul class="right">
                 <!-- Authentication Links -->
                 @if (Auth::guest())
                   <li><a href="{{ url('/login') }}">Login</a></li>
                   <li><a href="{{ url('/register') }}">Register</a></li>
                 @else
-                  <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                      {{ Auth::user()->name }} <span class="caret"></span>
+                  <li>
+                    <a href="#" class="dropdown-button" data-activates="dropdown1">
+                      {{ Auth::user()->name }} <i class="material-icons right">arrow_drop_down</i>
                     </a>
-
-                    <ul class="dropdown-menu" role="menu">
-                      <li><a href="{{ route('settings.edit') }}">Settings</a></li>
-                      <li>
-                        <a href="{{ url('/logout') }}"
-                          onclick="event.preventDefault();
-                          document.getElementById('logout-form').submit();">
-                          Logout
-                        </a>
-
-                        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-                          {{ csrf_field() }}
-                        </form>
-                      </li>
-                    </ul>
                   </li>
                 @endif
               </ul>
             </div>
-          </div>
-        </nav>
+          </nav>
+          <ul class="dropdown-content" id="dropdown1">
+            <li><a href="{{ route('settings.edit') }}">Settings</a></li>
+            <li>
+              <a href="{{ url('/logout') }}"
+                 onclick="event.preventDefault();
+                          document.getElementById('logout-form').submit();">
+                Logout
+              </a>
+
+              <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                {{ csrf_field() }}
+              </form>
+            </li>
+          </ul>
+        </div>
       @include('partials.guide')
       </header>
       <main>
@@ -88,6 +72,11 @@
 
     <!-- Scripts -->
     <script src="/js/app.js"></script>
+    <script>
+        $(document).ready(function () {
+            $(".dropdown-button").dropdown();
+        });
+    </script>
     @stack('script')
   </body>
 </html>
